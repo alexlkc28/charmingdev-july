9ac105e5ef6db60f12d65b894ed243e5d1b23982
Nguyen Nguyen <nguyen.nguyen@aboutknowledge.com>
2022-07-05 11:44:16 +0700

    rename order custom module

--- abk_order_custom/__manifest__.py
+++ abk_order_custom/__manifest__.py
@@ -25,10 +25,5 @@
     # always loaded
     'data': [
         'views/views.xml',
-        # 'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
