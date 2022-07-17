822fb669028620db896ab1ce38ce9b15050d81d3
Nguyen Nguyen <nguyen.nguyen@aboutknowledge.com>
2022-07-17 22:44:01 +0700

    Update Purchase Order Field

--- abk_order_custom/models/purchase_order.py
+++ abk_order_custom/models/purchase_order.py
@@ -46,7 +46,7 @@ class PurchaseOrderCustom(models.Model):
     abk_amendement_date_time = fields.Datetime('Amendement Date')
     abk_reclocation = fields.Char('reclocation')
     abk_attention = fields.Char('Attention')
-    abk_status = fields.Boolean('ABK Status')
+    abk_status = fields.Char('ABK Status')
     abk_payterm = fields.Char('Payterm')
     abk_paydesc = fields.Text('Payterm Description')
     abk_delterm = fields.Char('Delivery Term')
@@ -64,6 +64,11 @@ class PurchaseOrderCustom(models.Model):
         ('done', 'Locked'),
         ('cancel', 'Cancelled')
     ], string='Status', readonly=False, index=True, copy=False, default='draft', tracking=True)
+
+    partner_id = fields.Many2one('res.partner', string='Vendor', required=False, states=READONLY_STATES,
+                                 change_default=True, tracking=True,
+                                 domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+                                 help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
 
 
 
